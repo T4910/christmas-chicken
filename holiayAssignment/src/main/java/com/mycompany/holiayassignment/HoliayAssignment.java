83ab75b2caad0b7b528f59a668d51346ab624095
Emmanuel Taiwo <95715394+T4910@users.noreply.github.com>
2025-01-09 22:43:13 +0100

    Leave me Elvis

---
+++ holiayAssignment/src/main/java/com/mycompany/holiayassignment/HoliayAssignment.java
@@ -0,0 +1,147 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holiayassignment;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ttaiw
+ */
+public class HoliayAssignment {
+    public static void numberOneA(){
+        for(int a = 0; a < 4; a++){
+            System.out.println("****====****");
+        }
+    }
+    
+    public static void numberOneB(){
+        for(int i = 0; i < 4; i++){ 
+            for(int j = 0; j < 4; j++){
+                System.out.print("*");
+            }
+            for(int k = 0; k < 4; k++){
+                System.out.print("=");
+            }
+            for(int l = 0; l < 4; l++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+    public static void numberTwoA(){
+        for(int a = 0; a < 6; a++){
+            if(a < 3){
+                System.out.println("****======");
+            }
+            else {
+                System.out.println("==========");
+            }
+        }
+    }
+    
+    public static void numberTwoB(){
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 10; j++){
+                if(i < 3 && j < 4){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print("=");   
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+    
+    public static void numberThree(){
+        double[] numbers = {2, 5, 5, 9, 4, 7, 0, 9, 6, 11, 12, 8};
+        
+        double sum = 0;
+        
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        
+        double mean = sum/numbers.length;
+        
+        
+        double median;
+        
+        if(numbers.length % 2 == 0){
+            int middleIndex = numbers.length/2;
+            median = (numbers[middleIndex] + numbers[middleIndex+1])/2;
+        }
+        else {
+            int middleIndex = numbers.length/2;
+            median = numbers[middleIndex];
+        }
+        
+        int N = numbers.length;
+        
+        double devSum = 0;
+        for(int i = 0; i < numbers.length; i++){
+           devSum = numbers[i] - mean;
+        }
+        
+        double standardDeviation = Math.sqrt((devSum*devSum)/N);
+        
+        System.out.println("Mean: " + mean + "\nMedian: " + median + "\nStandard Deviation: " + standardDeviation);
+    }
+    
+    public static void numberFour(){
+        Scanner scanner = new Scanner(System.in);
+        String[] inputs = new String[10];
+        
+        for(int i = 0; i < 10; i++){
+            System.out.println("Please enter value for index " + i + ": ");
+            
+            inputs[i] = scanner.nextLine();
+        }
+        
+        for(int i = 0; i < 10; i++){
+            System.out.println("You entered the value: " + inputs[i] + " in index " + i);
+        }
+    }
+    
+    public static void numberFive(){
+        Scanner scanner = new Scanner(System.in);
+        String[][] inputs = new String[10][10];
+        
+        for(int i = 0; i < 10; i++){
+            String[] inner = new String[10];
+            
+            for(int j = 0; j < 10; j++){
+                System.out.println("Please enter value for index where X:" + i + ", Y:" + j + " : ");
+                inner[j] = scanner.nextLine();
+            }
+            
+            inputs[i] = inner;
+        }
+        
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                System.out.println("You entered the value: " + inputs[i][j] + " in X: " + i + ", Y:" + j);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Question 1a: \n");
+        numberOneA();
+        System.out.println("Question 1b: \n");
+        numberOneB();
+        System.out.println("Question 2a: \n");
+        numberTwoA();
+        System.out.println("Question 2b: \n");
+        numberTwoB();
+        System.out.println("Question 3: \n");
+        numberThree();
+        System.out.println("Question 4: \n");
+        numberFour();
+        System.out.println("Question 5: \n");
+        numberFive();
+    }
+}
