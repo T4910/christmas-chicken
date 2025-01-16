c483373a4e6b2db10a2a6207a34ed117105f1118
Emmanuel Taiwo <95715394+T4910@users.noreply.github.com>
2025-01-16 22:20:45 +0100

    Updated

--- holiayAssignment/src/main/java/com/mycompany/holiayassignment/HoliayAssignment.java
+++ holiayAssignment/src/main/java/com/mycompany/holiayassignment/HoliayAssignment.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Please note that this is a rough work & might have logic errors. Go through it first!!!
- */ 
+ */
 
 package com.mycompany.holiayassignment;
 import java.util.Scanner;
@@ -57,37 +56,66 @@ public class HoliayAssignment {
         }
     }
     
+    public static double[] Sort(double[] arr){
+//        2,5,4,1,3,6,3
+//        1,5,4,2,3,6,3
+        for(int i = 0; i < arr.length; i++){
+            double smallest = arr[i];
+            System.out.println(i + " "+ arr[i]);
+            for(int j = i; j < arr.length; j++){
+                double temp;
+                if(arr[j] < smallest){
+                    temp = smallest;
+                    
+                    smallest = arr[j];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                
+                System.out.println("Debog => smallest: " + smallest + "  arr[i]: " + arr[i] + "  arr[j]: " + arr[j] + "  ij: " + i + " " +  j);
+            }
+            System.out.println("\n");
+        }
+        
+        return arr;
+    }
+    
     public static void numberThree(){
-        double[] numbers = {2, 5, 5, 9, 4, 7, 0, 9, 6, 11, 12, 8};
+        double[] numbers = {2,5,4,1,3,6,3,7};
+//        1,2,3,3,4,5,6,7
+        
+        double sortedNumbers[] = Sort(numbers.clone());
+        
+        System.out.println(sortedNumbers[0] + " " + sortedNumbers[1] + " " +  sortedNumbers[2] + " " + sortedNumbers[3] + " " + sortedNumbers[4]);
         
         double sum = 0;
         
-        for(int i = 0; i < numbers.length; i++){
-            sum += numbers[i];
+        for(int i = 0; i < sortedNumbers.length; i++){
+            sum += sortedNumbers[i];
         }
         
-        double mean = sum/numbers.length;
+        double mean = sum/sortedNumbers.length;
         
         
         double median;
         
-        if(numbers.length % 2 == 0){
-            int middleIndex = numbers.length/2;
-            median = (numbers[middleIndex] + numbers[middleIndex+1])/2;
+        if(sortedNumbers.length % 2 == 0){
+            int middleIndex = sortedNumbers.length/2;
+            median = (sortedNumbers[middleIndex] + sortedNumbers[middleIndex-1])/2;
         }
         else {
-            int middleIndex = numbers.length/2;
-            median = numbers[middleIndex];
+            int middleIndex = sortedNumbers.length/2;
+            median = sortedNumbers[middleIndex];
         }
         
-        int N = numbers.length;
+        int N = sortedNumbers.length;
         
         double devSum = 0;
-        for(int i = 0; i < numbers.length; i++){
-           devSum = numbers[i] - mean;
+        for(int i = 0; i < sortedNumbers.length; i++){
+           devSum += (sortedNumbers[i] - mean) * (sortedNumbers[i] - mean);
         }
         
-        double standardDeviation = Math.sqrt((devSum*devSum)/N);
+        double standardDeviation = Math.sqrt(devSum/N);
         
         System.out.println("Mean: " + mean + "\nMedian: " + median + "\nStandard Deviation: " + standardDeviation);
     }
@@ -130,19 +158,19 @@ public class HoliayAssignment {
     }
     
     public static void main(String[] args) {
-        System.out.println("Question 1a: \n");
-        numberOneA();
-        System.out.println("Question 1b: \n");
-        numberOneB();
-        System.out.println("Question 2a: \n");
-        numberTwoA();
-        System.out.println("Question 2b: \n");
-        numberTwoB();
-        System.out.println("Question 3: \n");
+//        System.out.println("Question 1a: \n");
+//        numberOneA();
+//        System.out.println("Question 1b: \n");
+//        numberOneB();
+//        System.out.println("Question 2a: \n");
+//        numberTwoA();
+//        System.out.println("Question 2b: \n");
+//        numberTwoB();
+//        System.out.println("Question 3: \n");
         numberThree();
-        System.out.println("Question 4: \n");
-        numberFour();
-        System.out.println("Question 5: \n");
-        numberFive();
+//        System.out.println("Question 4: \n");
+//        numberFour();
+//        System.out.println("Question 5: \n");
+//        numberFive();
     }
 }
